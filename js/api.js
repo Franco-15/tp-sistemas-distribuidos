ac6accd3046dceeeb519b63f866a79f748c36396
NahuelBarriga <nahuel.barriga.nb@gmail.com>
2024-11-01 01:55:46 -0300

    Implementacion botones de edicion y delete de animal
    Se agregaron los botones para editar y borrar animales, falta la implementacion para que le pegue a la API

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-
+//import axios from 'axios';
 //* creo que aca deberian ir todos los llamados a la api asi centralizo todo lo del back en algun lado 
 //? deberia ser asi lo de arriba? 
 
@@ -9,9 +9,9 @@ export function getAnimals() {
     //! funcion solo de testeo, borrar despues
     // Datos simulados de animales
     const mockData = [
-        { id: 1, nombre: "Animal 1", description: "Descripción del Animal 1" },
-        { id: 2, nombre: "Animal 2", description: "Descripción del Animal 2" },
-        { id: 3, nombre: "Animal 3", description: "Descripción del Animal 3" }
+        { id: '11:5e:e7:84:c4:f6', nombre: "Grego", description: "Vaca" },
+        { id: '7c:0a:3f:83:db:93', nombre: "Alan", description: "Goat" },
+        { id: 'c2:5a:3d:ae:10:28', nombre: "Tomi", description: "Gallina" }
     ];
 
     
@@ -46,6 +46,25 @@ export function getAnimals() {
         */
 }
 
+export function PostAnimal(id, nombre, description) {
+    // axios.post('')
+    //todo: enviar aca los datos del animal para el post y usar el res para determinar si llego bien o no
+    //todo en el caso que llegue bien, disparar el get
+}
+
+export function PatchAnimal(id, nombre, description) {
+    // axios.post('') en la ruta tiene que ir el id
+    //todo: enviar aca los datos del animal para el post y usar el res para determinar si llego bien o no
+    //todo en el caso que llegue bien, disparar el get
+}
+
+export function DeleteAnimal(id) {
+    // axios.post('') en la ruta tiene que ir el id
+    //todo: enviar aca los datos del animal para el post y usar el res para determinar si llego bien o no
+    
+    //todo en el caso que llegue bien, disparar el get
+}
+
 export function getPtosControl() { 
     const mockData = [
         { id: 1, nombre: "pto 1", description: "agua" },
@@ -58,4 +77,5 @@ export function getPtosControl() {
         setTimeout(() => resolve(mockData), 500); // Simula una espera de 500ms (no se pq es esto pero chatgpt)
     });
 }
+
 
--- js/pages/AnimalsPage.js
+++ js/pages/AnimalsPage.js
@@ -1,6 +1,6 @@
 //todo: ver si no pasa a ser 'HomePage.js'
 
-import { getAnimals } from "../api.js";
+import { getAnimals, PatchAnimal, PostAnimal } from "../api.js";
 
 
 export function loadAnimalPage() {
@@ -9,6 +9,8 @@ export function loadAnimalPage() {
     renderAnimalsArray().then(animalArray => {
         const app = document.getElementById('app');
 
+
+        //todo ver como centrar los input del popup
         app.innerHTML = `
             <h2>Lista de Animales</h2>
             <ul>
@@ -17,47 +19,219 @@ export function loadAnimalPage() {
                 `).join('')}
             </ul>
             <button id="addAnimalButton">Agregar Animal</button>
+            <button id="editAnimalButton">Editar Animal</button>
+            <button id="deleteAnimalButton">Eliminar Animal</button>
             
-            <div id="popupForm" style="display: none; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); 
+
+            <!--popup para agregar-->
+            <div id="popupAddForm" style="display: none; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); 
                 background-color: white; padding: 20px; border: 1px solid #ccc; z-index: 1000;">
                 <h3>Agregar Nuevo Animal</h3>
-                <form id="animalForm">
-                    <label for="animalName">Nombre:</label>
-                    <input type="text" id="animalName" name="nombre" required><br><br>
-                    <label for="animalDescription">Descripción:</label>
-                    <input type="text" id="animalDescription" name="description" required><br><br>
-                    <button type="submit">Guardar</button>
+                <form id="animalAddForm">
+                    <label for="animalAddId">ID:</label> 
+                    <input type="text" id="animalAddId" name="Id" required><br><br>
+                    <label for="animalAddName">Nombre:</label>
+                    <input type="text" id="animalAddName" name="nombre" required><br><br>
+                    <label for="animalAddDescription">Descripción:</label>
+                    <input type="text" id="animalAddDescription" name="description" required><br><br>
+                    <button type="submit" id = "submitAdd">Guardar</button>
                     <button type="button" id="closeButton">Cerrar</button>
                 </form>
             </div>
+            
+            <!--popup para editar-->
+            <div id="popupEditForm" style="display: none; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); 
+                background-color: white; padding: 20px; border: 1px solid #ccc; z-index: 1000;">
+                <h3>Editar animal</h3>
+                <form id="animalEditForm">
+                    <label for="animalEditId">ID:</label> 
+                    <select id="animalSelectEdit">
+                        <option value="">-- Selecciona un animal --</option>
+                        ${animalArray.map(animal => `
+                            <option value="${animal.id}">${animal.id}</option>
+                        `).join('')}
+                    </select><br><br>
+                    
+                    <label for="animalEditName">Nombre:</label>
+                    <input type="text" id="animalEditName" name="nombre" required><br><br>
+                    <label for="animalEditDescription">Descripción:</label>
+                    <input type="text" id="animalEditDescription" name="description" required><br><br>
+                    <button type="submit" id = "submitEdit">Guardar</button>
+                    <button type="button" id="closeEditButton">Cerrar</button>
+                </form>
+            </div>
 
-            <>
+            <!--popup para eliminar-->
+            <div id="popupDeleteForm" style="display: none; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); 
+                background-color: white; padding: 20px; border: 1px solid #ccc; z-index: 1000;">
+                <h3>Eliminar animal</h3>
+                <form id="animalDeleteForm">
+                    <label for="animalDeleteId">ID:</label> 
+                    <select id="animalSelectDelete">
+                        <option value="">-- Selecciona un animal --</option>
+                        ${animalArray.map(animal => `
+                            <option value="${animal.id}">${animal.id}</option>
+                        `).join('')}
+                    </select><br><br>
+                    
+                    <label for="animalDeleteName">Nombre:</label>
+                    <input type="text" id="animalDeleteName" name="nombre" required readonly><br><br>
+                    <label for="animalDeleteDescription">Descripción:</label>
+                    <input type="text" id="animalDeleteDescription" name="description" required readonly><br><br>
+                    <button type="submit" id = "submitDelete">Guardar</button>
+                    <button type="button" id="closeDeleteButton">Cerrar</button>
+                </form>
+            </div>
+
+
+            <!--esto es para hacer el fondo mas oscuro-->
             <div id="popupOverlay" style="display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; 
                 background-color: rgba(0, 0, 0, 0.5); z-index: 999;"></div>
         `;
         
         // eventos
         const addAnimalButton = document.getElementById('addAnimalButton');
-        const popupForm = document.getElementById('popupForm');
+        const editAnimalButton = document.getElementById('editAnimalButton');
+        const deleteAnimalButton = document.getElementById('deleteAnimalButton');
+        const popupAddForm = document.getElementById('popupAddForm');
+        const popupEditForm = document.getElementById('popupEditForm');
         const popupOverlay = document.getElementById('popupOverlay');
         const closeButton = document.getElementById('closeButton');
-    
-        // popup
+                    
+
+        
+        // popup para agregar
         addAnimalButton.addEventListener('click', () => {
-            popupForm.style.display = 'block';
+            popupAddForm.style.display = 'block';
             popupOverlay.style.display = 'block';
         });
     
+
+        // popup para editar
+        editAnimalButton.addEventListener('click', () => {
+            popupEditForm.style.display = 'block';
+            popupOverlay.style.display = 'block';
+        });
+
+         // popup para eliminar
+         deleteAnimalButton.addEventListener('click', () => {
+            popupDeleteForm.style.display = 'block';
+            popupOverlay.style.display = 'block';
+        });
+
+        //esto rellena los valores para el editado
+        animalSelectEdit.addEventListener('change', () => {
+            const selectedAnimalId = animalSelectEdit.value;
+            console.log(selectedAnimalId)
+            if (selectedAnimalId) {
+                const selectedAnimal = animalArray.find(animal => animal.id.toString() === selectedAnimalId);
+                if (selectedAnimal) {
+                    console.log(selectedAnimal)
+                    animalEditName.value = selectedAnimal.nombre;
+                    animalEditDescription.value = selectedAnimal.description;
+                    
+                }
+                else {
+                    console.log('no encontre')
+                }
+            } else {
+                animalName.value = '';
+                animalDescription.value = '';
+            }
+        });
+
+        //esto rellena los valores para borrar el animal
+        animalSelectDelete.addEventListener('change', () => {
+            const selectedAnimalId = animalSelectDelete.value;
+            console.log(selectedAnimalId)
+            if (selectedAnimalId) {
+                const selectedAnimal = animalArray.find(animal => animal.id.toString() === selectedAnimalId);
+                if (selectedAnimal) {
+                    console.log(selectedAnimal)
+                    animalDeleteName.value = selectedAnimal.nombre;
+                    animalDeleteDescription.value = selectedAnimal.description;
+                    
+                }
+                else {
+                    console.log('no encontre')
+                }
+            } else {
+                animalName.value = '';
+                animalDescription.value = '';
+            }
+        });
+
         // cierra pop-up cuando se hace click en el boton de cerrar o en el fondo
         closeButton.addEventListener('click', () => {
-            popupForm.style.display = 'none';
+            popupAddForm.style.display = 'none';
             popupOverlay.style.display = 'none';
         });
-    
+
+        closeEditButton.addEventListener('click', () => {
+            popupEditForm.style.display = 'none';
+            popupOverlay.style.display = 'none';
+        });
+        
+        closeDeleteButton.addEventListener('click', () => {
+            popupDeleteForm.style.display = 'none';
+            popupOverlay.style.display = 'none';
+        });
+
         popupOverlay.addEventListener('click', () => {
-            popupForm.style.display = 'none';
+            popupAddForm.style.display = 'none';
+            popupEditForm.style.display = 'none';
+            popupDeleteForm.style.display = 'none';
             popupOverlay.style.display = 'none';
         });
+
+    //manejo de los datos al apretar submit del formulario agregar (pegaria a la API)
+    animalAddForm.addEventListener('submit', (event) => {
+        event.preventDefault(); // evita que se recarge la pagina
+        
+        const id = animalAddId.value;
+        const nombre = animalAddName.value;
+        const description = animalAddDescription.value;
+
+        console.log("Datos del nuevo animal:", {id, nombre, description }); //para comprobar //!borrar
+
+        PostAnimal(id, nombre, description);
+
+        popupAddForm.style.display = 'none';
+        popupOverlay.style.display = 'none';
+    });
+
+    //manejo de los datos al apretar submit del formulario editar (pegaria a la API)
+    animalEditForm.addEventListener('submit', (event) => {
+        event.preventDefault(); // evita que se recarge la pagina
+        
+        const id = animalSelectEdit.value;
+        const nombre = animalEditName.value;
+        const description = animalEditDescription.value;
+
+        console.log("Animal editado:", {id, nombre, description }); //para comprobar //!borrar
+
+        PatchAnimal(id, nombre, description);
+
+        popupEditForm.style.display = 'none';
+        popupOverlay.style.display = 'none';
+    });
+
+    //manejo de los datos al apretar submit del formulario delete (pegaria a la API)
+    animalDeleteForm.addEventListener('submit', (event) => {
+        event.preventDefault(); // evita que se recarge la pagina
+        
+        const id = animalSelectDelete.value;
+        const nombre = animalDeleteName.value;
+        const description = animalDeleteDescription.value;
+
+        console.log("animal a eliminar:", {id, nombre, description }); //para comprobar //!borrar
+
+        DeleteAnimal(id);
+
+        popupEditForm.style.display = 'none';
+        popupOverlay.style.display = 'none';
+    });
+
     });
 }
 
