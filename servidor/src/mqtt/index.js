5ff1c81ff37807e0c76e96c1f34f77b279a07bba
julieta hernandez <hernandezjulieta2105@gmail.com>
2024-11-01 11:36:31 -0300

    idem

--- servidor/src/mqtt/index.js
+++ servidor/src/mqtt/index.js
@@ -1,7 +1,7 @@
-import mqttClient from './mqtt.js';
+import mqtt from './mqtt.js';
 import { handleMessage } from './msnHandler.js';
 
-mqttClient.on('message', (topic, message) => {
+mqtt.on('message', (topic, message) => {
     handleMessage(topic, message);
 });
 
