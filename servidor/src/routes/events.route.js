e9a043df26a7a6e04680d8a26881ecf0e67d642f
julieta hernandez <hernandezjulieta2105@gmail.com>
2024-11-09 14:44:35 -0300

    mas express

--- servidor/src/routes/events.route.js
+++ servidor/src/routes/events.route.js
@@ -1,3 +1,37 @@
+import express from 'express';
+
+const router = express.Router();
+const clients = [];
+
+router.get('/', (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    });
+
+    clients.push(res);
+
+    req.on('close', () => {
+        clients.splice(clients.indexOf(res), 1);
+    });
+});
+
+
+export const sendSSE = (data) => {
+    clients.forEach((client) => {
+        client.write(`data: ${JSON.stringify(data)}\n\n`);
+    });
+};
+
+export default router;
+
+
+
+
+/*
 const clients = [];
 
 export const eventsRoute = (req, res) => {
@@ -14,3 +48,5 @@ export const sendSSE = (data) => {
         client.write(`data: ${JSON.stringify(data)}\n\n`);
     });
 }
+
+*/
--- servidor/src/routes/login.route.js
+++ servidor/src/routes/login.route.js
@@ -1,5 +1,20 @@
-import { validUser } from '../controllers/admins.controller.js';
+import express from 'express';
+import { validUser } from '../controllers/admin.controllers.js';
+
+const router = express.Router();
+
+// Ruta para el login, manejada por validUser
+router.post('/login', (req, res) => { //todo: chequear 
+    validUser(req, res);
+});
+
+export default router;
+
+
+/*import { validUser } from '../controllers/admins.controller.js';
 
 export const loginRoute = (req, res) => {
     validUser(req,res)
 }
+
+*/
