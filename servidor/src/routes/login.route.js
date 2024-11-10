3795bb1c1876895e66e47aaa2f69cba41167d7f7
julieta hernandez <hernandezjulieta2105@gmail.com>
2024-11-10 17:11:47 -0300

    update

--- servidor/src/routes/login.route.js
+++ servidor/src/routes/login.route.js
@@ -4,17 +4,10 @@ import { validUser } from '../controllers/admins.controller.js';
 const router = express.Router();
 
 // Ruta para el login, manejada por validUser
-router.get('/', (req, res) => { //todo: chequear 
+router.post('/', (req, res) => { 
     validUser(req, res);
 });
 
 export default router;
 
 
-/*import { validUser } from '../controllers/admins.controller.js';
-
-export const loginRoute = (req, res) => {
-    validUser(req,res)
-}
-
-*/
